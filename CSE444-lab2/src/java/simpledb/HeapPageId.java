5efe13ee99d2859ee5be15d38c0d3b929f548efd
mrmiywj <jsyangwenjie@gmail.com>
2016-02-10 18:14:53 +0800

    lab2 ex1 predicate filter done

---
+++ CSE444-lab2/src/java/simpledb/HeapPageId.java
@@ -0,0 +1,82 @@
+package simpledb;
+
+/** Unique identifier for HeapPage objects. */
+public class HeapPageId implements PageId {
+
+    /**
+     * Constructor. Create a page id structure for a specific page of a
+     * specific table.
+     *
+     * @param tableId The table that is being referenced
+     * @param pgNo The page number in that table.
+     */
+	private int tid;
+	private int pgNo;
+    public HeapPageId(int tableId, int pgNo) {
+        // some code goes here
+    	this.tid = tableId;
+    	this.pgNo = pgNo;
+    }
+
+    /** @return the table associated with this PageId */
+    public int getTableId() {
+        // some code goes here
+        return tid;
+    }
+
+    /**
+     * @return the page number in the table getTableId() associated with
+     *   this PageId
+     */
+    public int pageNumber() {
+        // some code goes here
+        return pgNo;
+    }
+
+    /**
+     * @return a hash code for this page, represented by the concatenation of
+     *   the table number and the page number (needed if a PageId is used as a
+     *   key in a hash table in the BufferPool, for example.)
+     * @see BufferPool
+     */
+    public int hashCode() {
+        // some code goes here
+        return tid+pgNo;
+    }
+
+    /**
+     * Compares one PageId to another.
+     *
+     * @param o The object to compare against (must be a PageId)
+     * @return true if the objects are equal (e.g., page numbers and table
+     *   ids are the same)
+     */
+    public boolean equals(Object o) {
+        // some code goes here
+    	if (o instanceof HeapPageId)
+    	{
+    		HeapPageId hpid = (HeapPageId) o;
+    		if (hpid.getTableId() == this.getTableId() && hpid.pageNumber() == this.pageNumber())
+    			return true;
+    		else 
+    			return false;
+    	}
+        return false;
+    }
+
+    /**
+     *  Return a representation of this object as an array of
+     *  integers, for writing to disk.  Size of returned array must contain
+     *  number of integers that corresponds to number of args to one of the
+     *  constructors.
+     */
+    public int[] serialize() {
+        int data[] = new int[2];
+
+        data[0] = getTableId();
+        data[1] = pageNumber();
+
+        return data;
+    }
+
+}
---
+++ CSE444-lab2/src/java/simpledb/SeqScan.java
@@ -0,0 +1,157 @@
+package simpledb;
+
+import java.util.*;
+
+/**
+ * SeqScan is an implementation of a sequential scan access method that reads
+ * each tuple of a table in no particular order (e.g., as they are laid out on
+ * disk).
+ */
+public class SeqScan implements DbIterator {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates a sequential scan over the specified table as a part of the
+     * specified transaction.
+     * 
+     * @param tid
+     *            The transaction this scan is running as a part of.
+     * @param tableid
+     *            the table to scan.
+     * @param tableAlias
+     *            the alias of this table (needed by the parser); the returned
+     *            tupleDesc should have fields with name tableAlias.fieldName
+     *            (note: this class is not responsible for handling a case where
+     *            tableAlias or fieldName are null. It shouldn't crash if they
+     *            are, but the resulting name can be null.fieldName,
+     *            tableAlias.null, or null.null).
+     */
+    private TransactionId tid;
+    private int tableid;
+    private String tableAlias;
+    private DbFileIterator it;
+    private DbFile dbf;
+    boolean isOpen;
+    public SeqScan(TransactionId tid, int tableid, String tableAlias) {
+        // some code goes here
+    	this.tid = tid;
+    	this.tableid = tableid;
+    	this.tableAlias = tableAlias;
+    }
+
+    /**
+     * @return
+     *       return the table name of the table the operator scans. This should
+     *       be the actual name of the table in the catalog of the database
+     * */
+    public String getTableName() {
+        return Database.getCatalog().getTableName(tableid);
+    }
+    
+    /**
+     * @return Return the alias of the table this operator scans. 
+     * */
+    public String getAlias()
+    {
+        // some code goes here
+        return tableAlias;
+    }
+
+    /**
+     * Reset the tableid, and tableAlias of this operator.
+     * @param tableid
+     *            the table to scan.
+     * @param tableAlias
+     *            the alias of this table (needed by the parser); the returned
+     *            tupleDesc should have fields with name tableAlias.fieldName
+     *            (note: this class is not responsible for handling a case where
+     *            tableAlias or fieldName are null. It shouldn't crash if they
+     *            are, but the resulting name can be null.fieldName,
+     *            tableAlias.null, or null.null).
+     */
+    public void reset(int tableid, String tableAlias) {
+        // some code goes here
+    	this.tableid = tableid;
+    	this.tableAlias = tableAlias;
+    	isOpen = false;
+    }
+
+    public SeqScan(TransactionId tid, int tableid) {
+        this(tid, tableid, Database.getCatalog().getTableName(tableid));
+    }
+
+    public void open() throws DbException, TransactionAbortedException {
+        // some code goes here
+    	dbf = Database.getCatalog().getDatabaseFile(tableid);
+    	it = dbf.iterator(tid);
+    	it.open();
+    	isOpen = true;
+    }
+
+    /**
+     * Returns the TupleDesc with field names from the underlying HeapFile,
+     * prefixed with the tableAlias string from the constructor. This prefix
+     * becomes useful when joining tables containing a field(s) with the same
+     * name.
+     * 
+     * @return the TupleDesc with field names from the underlying HeapFile,
+     *         prefixed with the tableAlias string from the constructor.
+     */
+    public TupleDesc getTupleDesc() {
+        // some code goes here
+    	TupleDesc td = this.dbf.getTupleDesc();
+    	int l = td.getSize();
+    	Type[] tarr = new Type[l];
+    	String[] farr = new String[l];
+    	for (int i = 0;i < l; ++i)
+    	{
+    		tarr[i] = td.getFieldType(i);
+    		farr[i] = this.tableAlias + "." + td.getFieldName(i);
+    	}
+        return new TupleDesc(tarr,farr);
+    }
+
+    public boolean hasNext() throws TransactionAbortedException, DbException {
+        // some code goes here
+    	if (!isOpen)
+    		throw new IllegalStateException();
+    	if (it == null)
+    		return false;
+    	else
+    		return it.hasNext();
+        //return false;
+    }
+
+    public Tuple next() throws NoSuchElementException,
+            TransactionAbortedException, DbException {
+        // some code goes here
+    	if (!isOpen)
+    		throw new IllegalStateException();
+    	if (it == null)
+    		throw new IllegalStateException();
+    	else
+    	{
+    		Tuple t = it.next();
+    		if (t == null)
+    			throw new NoSuchElementException("No next tuple~");
+    		else
+    			return t;
+    	}
+    		//return it.next();
+       // return null;
+    }
+
+    public void close() {
+        // some code goes here
+    	it = null;
+    	isOpen = false;
+    }
+
+    public void rewind() throws DbException, NoSuchElementException,
+            TransactionAbortedException {
+        // some code goes here
+    	it.close();
+    	it.open();
+    }
+}
